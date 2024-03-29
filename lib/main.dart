5823414af0d2be8e49b33284409c59af64c15647
@franz <franzcarhuaricram@prima.com.pe>
2024-03-28 21:37:01 -0500

    Fix Image.network(...) throws HandshakeException -> Override createHttpClient

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_widgets_app/config/theme/app_theme.dart';
 import 'package:flutter_widgets_app/config/router/app_router.dart';
 
+class MyHttpOverrides extends HttpOverrides{
+  @override
+  HttpClient createHttpClient(SecurityContext? context){
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
+  }
+}
+
 void main() {
+  HttpOverrides.global = MyHttpOverrides();
   runApp(const MainApp());
 }
 
